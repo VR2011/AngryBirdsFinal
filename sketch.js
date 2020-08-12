ae8ca4bb5329a5131b4d702288d5f04aa6c4c6b0
VR2011 <64109448+VR2011@users.noreply.github.com>
2020-08-11 21:56:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,14 +47,14 @@ function setup(){
 }
 
 function draw(){
-    if(backgroundImg){
+    if(backgroundImg)
         background(backgroundImg);
     
         noStroke();
         textSize(35)
         fill("white")
         text("Score  " + score, width-300, 50)
-    }
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -81,9 +81,9 @@ function draw(){
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -94,9 +94,7 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-    bird.trajectory = [];
-    Matter.Body.setPosition(bird.body, {x: 200, y: 50});
-    slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
 }
 
